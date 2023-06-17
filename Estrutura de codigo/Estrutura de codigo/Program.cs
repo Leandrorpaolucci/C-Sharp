10ab1a60a8b4718ee7a94520b9eb20ac9d011b51
Leandroydc <leandroydc2@gmail.com>
2023-06-17 04:19:51 -0300

    Avançando com os estudos C#
    Imprimir textos na tela

---
+++ Estrutura de codigo/Estrutura de codigo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_codigo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá C#");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Imprimindo textos/Imprimindo textos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imprimindo_textos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo!"); //escrever uma linha
+            Console.WriteLine("Tudo bem com você? "); //escrever uma linha
+
+            
+
+            Console.Write("Meu nome");
+            Console.Write("é");
+            Console.Write("\r\n");
+            Console.Write("João");
+
+            Console.ReadKey(); //Aguardar um usuário digitar uma tecla!
+        }
+    }
+}
